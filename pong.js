5080e2114b228b538e088f9949dc725a81e0ec2e
Aristides Neto <aristidesjbn@gmail.com>
2023-05-23 14:33:50 -0300

    boraaaa

--- pong.js
+++ pong.js
@@ -1,16 +1,43 @@
-
-var button = document.getElementById("idbutton")
-
-button.addEventListener("click", function(){
-
-    var campo = document.getElementById("idcampo")
-    if(campo.style.display === "none"){
-        campo.style.display = "block"
-    }else{
-        campo.style.display = "none"
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("idbutton");
+    var campo = document.getElementById("idcampo");
+    var infocard = document.getElementById("idcard");
+  
+    button.addEventListener("click", function botao() {
+      if (campo.style.display === "block") {
+        campo.style.display = "none";
+        button.textContent = "Jogar Pong";
+      } else {
+        campo.style.display = "block";
+        button.textContent = "Parar jogo";
+      }
+    });
+  
+    var links = document.querySelectorAll("a");
+    links.forEach(function(link) {
+      link.addEventListener("click", function(event) {
+        event.preventDefault();
+        var color = this.getAttribute("data-color");
+        fJS(color);
+      });
+    });
+  
+    function fJS(color) {
+      infocard.style.display = "block";
+      switch (color) {
+        case "orange":
+          infocard.style.backgroundColor = "orange";
+          break;
+        case "blue":
+          infocard.style.backgroundColor = "rgb(65, 160, 255)";
+          break;
+        case "yellow":
+          infocard.style.backgroundColor = "yellow";
+          break;
+        default:
+          infocard.style.backgroundColor = "white";
+          break;
+      }
     }
-})
-
-
-console.log(campo);
-console.log(button);
+  });
+  
