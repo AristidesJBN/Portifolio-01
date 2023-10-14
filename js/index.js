3157aec1f41d249cd9f2a92fc68c8ca6655ab3d2
Aristides Neto <aristidesjbn@gmail.com>
2023-10-14 05:07:16 -0300

    perfil com pong real oficial

--- js/index.js
+++ js/index.js
@@ -3,12 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
     var btn2 = document.getElementById("btn2");
     var card1 = document.getElementById("card1");
     var card2 = document.getElementById("card2");
+    var btnPong = document.getElementById("btnPong");
 
     function ocultarTodosCartoes() {
       document.getElementById('card1').style.display = 'none';
       document.getElementById('card2').style.display = 'none';
       document.getElementById('card3').style.display = 'none';
       document.getElementById('cardConstruct').style.display = 'none';
+      document.getElementById('cardPong').style.display = 'none';
     }
       
     btn1.addEventListener("click", function botao() {
@@ -46,6 +48,16 @@ document.addEventListener("DOMContentLoaded", function() {
         cardConstruct.style.display = "block";
       }
     });
+
+    btnPong.addEventListener("click", function botao() {
+      if (cardPong.style.display === "block") {
+        cardPong.style.display = "none";
+        btnPong.textContent = "Jogar Pong";
+      } else {
+        cardPong.style.display = "block";
+        btnPong.textContent = "Fechar jogo";
+      }
+    });
   }
 );
 
@@ -79,3 +91,100 @@ function playPK() {
 function playPAC() {
   window.open(urlPAC, '_blank');
 }
+
+//Pong
+
+const leftPaddle = document.getElementById('leftPaddle');
+const rightPaddle = document.getElementById('rightPaddle');
+const ball = document.getElementById('ball');
+
+const containerWidth = 584;
+const containerHeight = 350;
+
+let ballX = containerWidth / 2;
+let ballY = containerHeight / 2;
+let ballSpeedX = 5;
+let ballSpeedY = 5;
+
+const paddleSpeed = 10;
+
+const keys = {};
+
+let player1Score = 0;
+let player2Score = 0;
+
+const player1ScoreElement = document.getElementById('player1Score');
+const player2ScoreElement = document.getElementById('player2Score');
+
+document.addEventListener('keydown', (event) => {
+    keys[event.key] = true;
+});
+
+document.addEventListener('keyup', (event) => {
+    keys[event.key] = false;
+});
+
+function update() {
+    if (keys['w'] && leftPaddle.offsetTop > 0) {
+        leftPaddle.style.top = (leftPaddle.offsetTop - paddleSpeed) + 'px';
+    }
+
+    if (keys['s'] && leftPaddle.offsetTop + leftPaddle.clientHeight < containerHeight) {
+        leftPaddle.style.top = (leftPaddle.offsetTop + paddleSpeed) + 'px';
+    }
+
+    if (keys['ArrowUp'] && rightPaddle.offsetTop > 0) {
+        rightPaddle.style.top = (rightPaddle.offsetTop - paddleSpeed) + 'px';
+    }
+
+    if (keys['ArrowDown'] && rightPaddle.offsetTop + rightPaddle.clientHeight < containerHeight) {
+        rightPaddle.style.top = (rightPaddle.offsetTop + paddleSpeed) + 'px';
+    }
+
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+
+    if (ballY <= 0 || ballY + ball.clientHeight >= containerHeight) {
+        ballSpeedY = -ballSpeedY;
+    }
+
+    if (
+        ballX <= leftPaddle.offsetLeft + leftPaddle.clientWidth &&
+        ballY >= leftPaddle.offsetTop &&
+        ballY <= leftPaddle.offsetTop + leftPaddle.clientHeight
+    ) {
+        ballSpeedX = -ballSpeedX;
+    }
+
+    if (
+        ballX + ball.clientWidth >= rightPaddle.offsetLeft &&
+        ballY >= rightPaddle.offsetTop &&
+        ballY <= rightPaddle.offsetTop + rightPaddle.clientHeight
+    ) {
+        ballSpeedX = -ballSpeedX;
+    }
+
+    if (ballX <= 0) {
+        ballX = containerWidth / 2;
+        ballY = containerHeight / 2;
+        ballSpeedX = 5;
+        ballSpeedY = 5;
+        player2Score++;
+        player2ScoreElement.textContent = `Player 2: ${player2Score}`;
+    }
+
+    if (ballX >= containerWidth - ball.clientWidth) {
+        ballX = containerWidth / 2;
+        ballY = containerHeight / 2;
+        ballSpeedX = -5;
+        ballSpeedY = 5;
+        player1Score++;
+        player1ScoreElement.textContent = `Player 1: ${player1Score}`;
+    }
+
+    ball.style.left = ballX + 'px';
+    ball.style.top = ballY + 'px';
+
+}
+
+setInterval(update, 20);
